c59f9db764dd4dc7e769510d3ba04e7308caae53
maximiliansmidt <maxismidt@icloud.com>
2024-07-30 19:46:02 +0200

    [Feature/25] separation in home view
    resolves #25

--- frontend/src/app/features/home/tutor-home/tutor-home.component.ts
+++ frontend/src/app/features/home/tutor-home/tutor-home.component.ts
@@ -3,25 +3,39 @@ import {UserService} from "../../../core/services/user.service";
 import {TranslatePipe} from "../../../shared/pipes/translate.pipe";
 import {CourseCardComponent} from "./course-card/course-card.component";
 import {SimpleCourseInstance} from "../../course/models/course.model";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {TranslationService} from "../../../shared/services/translation.service";
 
 @Component({
   selector: 'ms-tutor-home',
   templateUrl: './tutor-home.component.html',
   standalone: true,
-  imports: [TranslatePipe, CourseCardComponent]
+  imports: [TranslatePipe, CourseCardComponent, DropdownModule, FormsModule]
 })
 export class TutorHomeComponent implements OnInit {
   courseInstances: SimpleCourseInstance[] = [];
+  yearItems: string[] = [];
+  selectedYear: string | undefined;
+  allString: string;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+              private translationService: TranslationService) {
+    this.allString = this.translationService.translate('home.tutorHome.all');
   }
 
   ngOnInit() {
     this.userService.getUserCourses().subscribe({
       next: courseInstances => {
         this.courseInstances = courseInstances;
+        this.fillItems();
       }
     });
   }
 
+  private fillItems() {
+    this.yearItems = this.courseInstances.map( ci => ci.year.toString());
+    this.yearItems.push(this.allString);
+    this.selectedYear = this.allString;
+  }
 }
